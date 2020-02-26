267e7d4ea1cf3b7c67e8c2ce9440f8fd3fb7406c
Egor Khindikaynen <ambertape.music@gmail.com>
2020-02-26 13:12:09 +0300

    FLUID-64 Fluid.Core: Добавлен проект песочницы, поправлен nuget.config.

---
+++ sources/Fluid.Core.Sandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fluid.Core.Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Utils.Serialization.Json.WriteToFile("1.asd", new object());
+        }
+    }
+}
