90ad095ee9d3832c4faffb6d11e6c8410556546d
Egor Khindikaynen <khek@ambertape.ru>
2022-01-23 20:38:44 +0300

    Removed redundant classes.

---
+++ sources/Waves.Core/Base/WavesObservablePlugin.cs
@@ -0,0 +1,12 @@
+using Waves.Core.Base.Interfaces;
+
+namespace Waves.Core.Base;
+
+/// <summary>
+/// Observable plugin.
+/// </summary>
+public class WavesObservablePlugin :
+    WavesObservableObject,
+    IWavesObservablePlugin
+{
+}
