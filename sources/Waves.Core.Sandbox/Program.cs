0c95eca1002b9396ea562c8456229e84386c4b84
Egor Khindikaynen <khek@ambertape.ru>
2022-03-17 19:51:16 +0300

    Minor fix.

--- sources/Waves.Core.Sandbox/Program.cs
+++ sources/Waves.Core.Sandbox/Program.cs
@@ -1,14 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using Microsoft.Extensions.Logging;
 using Waves.Core;
-using Waves.Core.Base;
 using Waves.Core.Sandbox.Services;
 
 var core = new WavesCore();
 await core.StartAsync();
 await core.BuildContainerAsync();
 
+var logger = await core.GetInstanceAsync<ILogger<Program>>();
+logger.LogInformation($"Program started");
+
 var service = await core.GetInstanceAsync<SampleConfigurableService>();
 var value = service.TestValue;
+
+logger.LogInformation($"Test value: {value}");
+logger.LogInformation($"Press ENTER to exit...");
 
 Console.ReadLine();
