017555be99dea2376bfc6c2c346622b37a23c3f7
Egor Khindikaynen <e.khindikainen@scalaxi.com>
2022-12-13 22:21:03 +0300

    Additional service tests.

--- sources/Waves.Core.Tests/CoreTests.cs
+++ sources/Waves.Core.Tests/CoreTests.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Autofac;
 using Microsoft.Extensions.Logging;
 using Waves.Core.Services.Interfaces;
+using Waves.Core.Tests.Services;
 
 namespace Waves.Core.Tests;
 
@@ -70,5 +71,80 @@ public class CoreTests
         var serviceProvider = container.Resolve<IWavesServiceProvider>();
         var logger = await serviceProvider.GetInstanceAsync<ILogger<WavesCore>>();
         Assert.That(logger, Is.Not.Null);
+    }
+
+    /// <summary>
+    /// Tests that <see cref="WavesCore"/> test service resolves successfully.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+    [Test]
+    public async Task WavesCore_ResolveTestService_ResolvesSuccessfully()
+    {
+        var core = new WavesCore();
+        await core.StartAsync();
+        var container = await core.BuildContainerAsync();
+        var serviceProvider = container.Resolve<IWavesServiceProvider>();
+        var service = await serviceProvider.GetInstanceAsync<TestService>();
+        Assert.That(service, Is.Not.Null);
+    }
+
+    /// <summary>
+    /// Tests that <see cref="WavesCore"/> initializable test service resolves successfully.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+    [Test]
+    public async Task WavesCore_ResolveInitializableTestService_TestValueEqualsExpected()
+    {
+        var core = new WavesCore();
+        await core.StartAsync();
+        var container = await core.BuildContainerAsync();
+        var serviceProvider = container.Resolve<IWavesServiceProvider>();
+        var service = await serviceProvider.GetInstanceAsync<TestInitializableService>();
+        Assert.That(service.TestValue, Is.EqualTo(50));
+    }
+
+    /// <summary>
+    /// Tests that <see cref="WavesCore"/> initializable test service resolves successfully.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+    [Test]
+    public async Task WavesCore_ResolveInitializableTestService_TestValueNotEqualsZero()
+    {
+        var core = new WavesCore();
+        await core.StartAsync();
+        var container = await core.BuildContainerAsync();
+        var serviceProvider = container.Resolve<IWavesServiceProvider>();
+        var service = await serviceProvider.GetInstanceAsync<TestInitializableService>();
+        Assert.That(service.TestValue, Is.Not.EqualTo(0));
+    }
+
+    /// <summary>
+    /// Tests that <see cref="WavesCore"/> configurable test service resolves successfully.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+    [Test]
+    public async Task WavesCore_ResolveConfigurableTestService_TestValueEqualsExpected()
+    {
+        var core = new WavesCore();
+        await core.StartAsync();
+        var container = await core.BuildContainerAsync();
+        var serviceProvider = container.Resolve<IWavesServiceProvider>();
+        var service = await serviceProvider.GetInstanceAsync<TestConfigurableService>();
+        Assert.That(service.TestValue, Is.EqualTo(25));
+    }
+
+    /// <summary>
+    /// Tests that <see cref="WavesCore"/> configurable test service resolves successfully.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+    [Test]
+    public async Task WavesCore_ResolveConfigurableTestService_TestValueNotEqualsZero()
+    {
+        var core = new WavesCore();
+        await core.StartAsync();
+        var container = await core.BuildContainerAsync();
+        var serviceProvider = container.Resolve<IWavesServiceProvider>();
+        var service = await serviceProvider.GetInstanceAsync<TestConfigurableService>();
+        Assert.That(service.TestValue, Is.Not.EqualTo(0));
     }
 }
